e569e0b8f2b7ef7af1e5a64698a2cb9281ac27ed
chryl <1015670600@qq.com>
2023-08-01 08:31:59 +0800

    Initial commit

---
+++ src/main/java/com/chryl/chrpinyin/ChrPinyinApplication.java
@@ -0,0 +1,13 @@
+package com.chryl.chrpinyin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChrPinyinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChrPinyinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chryl/chrpinyin/ChrPinyinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chryl.chrpinyin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChrPinyinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
